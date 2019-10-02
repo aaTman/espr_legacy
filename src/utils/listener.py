88fca376bff874c0aeb7b819b04b7c449dd6e974
Taylor Mandelbaum <taylorm@chaos.cm.cluster>
2019-10-01 22:48:52 -0400

    removed ftp access message from triggering on each run

--- src/utils/listener.py
+++ src/utils/listener.py
@@ -13,12 +13,13 @@ def monitor(server, directory, temp_store):
             pass
     ftp = FTP(server)
     ftp.login()
-    print('logged into ftp at ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    
     ftp.cwd(directory)
     new_date = ftp.nlst("-t")[0]
-    ftp.cwd(new_folder)
+    ftp.cwd(new_date)
     new_run = ftp.nlst("-t")[0]
     if new_run != old_run:
+        print('logged into ftp at ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         try:
             print('new: {}, old: {} '.format(new_run, old_run) + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         except ValueError:
@@ -27,7 +28,7 @@ def monitor(server, directory, temp_store):
         if any('.f168' in s for s in ftp.nlst()):
             print('beginning to retrieve files at ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S')) 
             retr_files(ftp,temp_store)
-            old_folder = new_folder
+            old_date = new_date
             old_run = new_run
             with open(log_directory + 'current_run.txt', "a+") as f:
                 f.write(new_date+'_'+new_run+'_\n')
