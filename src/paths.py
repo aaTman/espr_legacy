fbf675db07960909ea062b76c903e3711a6bd954
Taylor Mandelbaum <mandelbaum.taylor@gmail.com>
2020-09-29 10:46:19 -0400

    oops

--- src/paths.py
+++ src/paths.py
@@ -1,6 +1,6 @@
 log_directory = '/home/taylorm/espr/logs/'
 ftp = 'ftp.ncep.noaa.gov'
-ftp_dir = '/data/nccf/com/gens/prod/'
+ftp_dir = 'pub/data/nccf/com/gens/prod/'
 data_store = '/home/taylorm/espr/temp/'
 rfcst = '/home/taylorm/espr/reforecast/'
 fpath = '/E1/taylorm/espr/misc/comfortaa/Comfortaa-Regular.ttf'
